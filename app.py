3cd27b071c4c6a9ecac30c3739babca6c71a857e
Praphull Kumar <55136476+KPRAPHULL@users.noreply.github.com>
2021-12-18 19:54:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import joblib 
-st.title('Month vs Sales Prediction')            #title for the webapp
+st.title('Months vs Sales Prediction')            #title for the webapp
 forecast_model = joblib.load('forecast')        #load the model using joblib
 ip = st.slider("Pick a number of months", 0, 24)         #user input in streamlit 
 op = forecast_model.make_future_dataframe(periods = ip, freq = 'M')
 
 # Predict the graph
 predict = forecast_model.predict(op)  
-fig = forecast_model.plot(forecast)
+fig = forecast_model.plot(predict)
 st.pyplot(fig)                   
