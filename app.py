f474a48fbf533848d56203c829409b6bda5694a0
Praphull Kumar <55136476+KPRAPHULL@users.noreply.github.com>
2021-12-18 12:52:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import joblib 
+st.title('Moth vs Sales Prediction')            #title for the webapp
+forecast_model = joblib.load('forecast')        #load the model using joblib
+ip = st.slider("Pick a number of months", 0, 24)         #user input in streamlit 
+op = forecast_model.make_future_dataframe(periods = ip, freq = 'M')
+
+# Predict the graph
+predict = forecast_model.predict(op)  
+fig = model.plot(predict)
+st.write(predict)                    
